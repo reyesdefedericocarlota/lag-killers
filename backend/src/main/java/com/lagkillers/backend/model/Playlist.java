924f809f6dd213a29bc6b19966e0ddb4a2a72fb8
Borja <bvc91c@gmail.com>
2026-05-20 11:59:22 +0200

    pages actualizado

---
+++ backend/src/main/java/com/lagkillers/backend/model/Playlist.java
@@ -0,0 +1,37 @@
+package com.lagkillers.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "playlists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(name = "playlist_songs", joinColumns = @JoinColumn(name = "playlist_id"), inverseJoinColumns = @JoinColumn(name = "song_id"))
+    @JsonManagedReference
+    private List<Song> songs;
+}
