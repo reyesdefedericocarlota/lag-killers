1740432b35f1598b3981921759f9706d336ef137
Carlos <carlosmartindominguez2006@gmail.com>
2026-05-20 11:24:45 +0200

    feat: maquetacion completa de vistas y signals ya con usuarios,playlist etc todo hecho, primera version

---
+++ frontend/src/app/pages/canciones/canciones.spec.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-canciones',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './canciones.component.html',
+  styleUrls: ['./canciones.component.css']
+})
+export class CancionesComponent implements OnInit {
+  cancionForm!: FormGroup;
+
+  listaCanciones = [
+    { id: 1, titulo: 'Blinding Lights', artista: 'The Weeknd', duracion: '3:20' },
+    { id: 2, titulo: 'Starboy', artista: 'The Weeknd', duracion: '3:50' },
+    { id: 3, titulo: 'Save Your Tears', artista: 'The Weeknd', duracion: '3:35' }
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cancionForm = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2)]],
+      artista: ['', [Validators.required]],
+      duracion: ['', [Validators.required, Validators.pattern('^[0-9]+:[0-5][0-9]$')]]
+    });
+  }
+
+  guardarCancion(): void {
+    if (this.cancionForm.valid) {
+      const nuevaCancion = {
+        id: this.listaCanciones.length + 1,
+        ...this.cancionForm.value
+      };
+      this.listaCanciones.push(nuevaCancion);
+      this.cancionForm.reset();
+    } else {
+      alert('Por favor, rellena todos los campos correctamente.');
+    }
+  }
+}
