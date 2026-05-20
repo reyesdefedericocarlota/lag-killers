51f059aa8d0f37a18a414d073e9ca4bbc141f57a
Borja <bvc91c@gmail.com>
2026-05-20 11:07:15 +0200

    Router modificado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import{PlaylistsComponent} from '.pages/playlists/playlists.component';
+import{PlaylistFormComponent} from '.pages/playlist-form/playlist-form.component';
+import{PlaylistDetailComponent} from '.pages/playlist-detail/playlist-detail.component';
+export const routes: Routes = [
+  { path: '', redirectTo: 'playlists', pathMatch: 'full' },
 
-export const routes: Routes = [];
+  { path: 'playlists', component: PlaylistsComponent },
+
+  { path: 'playlists/nueva', component: PlaylistFormComponent },
+
+  { path: 'playlists/:id', component: PlaylistDetailComponent },
+];
